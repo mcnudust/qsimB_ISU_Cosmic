dda97b5e0dbdb537f42808527f3843962964700e
Carlos Bula <bulacarl@isu.edu>
2015-03-05 18:45:49 -0700

    first commit

---
+++ build/CMakeFiles/qsim.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bulacarl/solid/repository/qsim_B/qsim/qsim.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/qsim.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimDetector.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimDetector.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimDetectorConstruction.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimDetectorConstruction.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimDetectorHit.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimDetectorHit.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimEvent.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimEvent.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimEventAction.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimEventAction.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimIO.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimIO.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimMessenger.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimMessenger.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimOpticalPhysics.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimOpticalPhysics.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimPrimaryGeneratorAction.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimPrimaryGeneratorAction.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimRunAction.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimRunAction.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimScintDetector.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimScintDetector.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimScintDetectorHit.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimScintDetectorHit.cc.o"
+  "/home/bulacarl/solid/repository/qsim_B/qsim/src/qsimSteppingAction.cc" "/home/bulacarl/solid/repository/qsim_B/build/CMakeFiles/qsim.dir/src/qsimSteppingAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4_STORE_TRAJECTORY"
+  "G4VERBOSE"
+  "G4UI_USE"
+  "G4VIS_USE"
+  "G4UI_USE_TCSH"
+  "G4INTY_USE_QT"
+  "G4UI_USE_QT"
+  "G4VIS_USE_OPENGLQT"
+  "G4INTY_USE_XT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGL"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/qsim.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/qsim.dir/qsim.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimMessenger.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimScintDetector.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimSteppingAction.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimDetectorConstruction.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimDetector.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimDetectorHit.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimEvent.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimEventAction.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimIO.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimOpticalPhysics.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimRunAction.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimScintDetectorHit.cc.o"
+  "CMakeFiles/qsim.dir/src/qsimPrimaryGeneratorAction.cc.o"
+  "qsim.pdb"
+  "qsim"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/qsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
